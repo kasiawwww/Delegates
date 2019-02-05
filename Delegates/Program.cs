c1c1e8c768ecfb7475f41e0bf85ab6a63a2401d6
test <k.widlak@microsoft.wsei.edu.pl>
2019-02-05 17:47:47 +0100

    del

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -10,6 +10,22 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
+            Action<string> cw = Console.WriteLine;
+            Func<int> cr = Console.Read;
+            
+            cw("1");
+            cw += writeNumber;
+            cw("2");
+            cw -= Console.WriteLine;
+            cw("3");
+
+            cr();
+        }
+
+        static void writeNumber(string strNumber)
+        {
+            //logika
+            Console.Write($"Podany string jest liczbą {strNumber}");
         }
     }
 }
